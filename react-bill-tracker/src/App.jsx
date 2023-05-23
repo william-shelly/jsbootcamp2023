914f0f95d54a4f31bbe47b2f76dabe604c639fc2
William Shelly <wmshelly@mac.com>
2023-05-23 08:24:14 -0500

    Adding bill tracker

---
+++ react-bill-tracker/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container mx-auto my-4">
+        <div className="column-1">
+          <h1>Bill Tracker</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit inventore minima quas, libero porro aliquid aut blanditiis? Iste dolorem minus omnis sapiente, doloremque saepe aliquid libero corporis at ea modi.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
